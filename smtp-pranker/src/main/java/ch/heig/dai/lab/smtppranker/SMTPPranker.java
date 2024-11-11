6d49048bad1b94aa7cbb89f8519ad3227c56e531
SDY <dylan.stegmuller@heig-vd.ch>
2024-11-11 14:46:23 +0100

    Initialized project

---
+++ smtp-pranker/src/main/java/ch/heig/dai/lab/smtppranker/SMTPPranker.java
@@ -0,0 +1,10 @@
+package ch.heig.dai.lab.smtppranker;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
